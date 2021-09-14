291228203a931bb93e2f32f68b5c87c5f2b042d3
Tấn Lưu Trọng <tanluutrong2206@gmail.com>
2021-09-14 23:29:29 +0700

    add search event filter

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,21 @@
+import { useRouter } from "next/router";
+
+import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventSearch";
+import { getAllEvents } from "../../dummy-data";
+
 const EventsPage = () => {
-  return <div>List Events Page</div>;
+  const events = getAllEvents();
+  const router = useRouter();
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
+  return (
+    <>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />{" "}
+    </>
+  );
 };
 
 export default EventsPage;
