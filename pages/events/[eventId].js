4b6c6a4cb3d0a34d40abf869532c22f6af7e21b3
Tấn Lưu Trọng <tanluutrong2206@gmail.com>
2021-09-14 21:35:12 +0700

    add event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,31 @@
 import { useRouter } from "next/router";
+import EventContent from "../../components/event-detail/EventContent";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventSummary from "../../components/event-detail/EventSummary";
+import { getEventById } from "../../dummy-data";
 
 const DetailEventPage = () => {
   const router = useRouter();
-  return <div>Detail Event Page with id: {router.query.eventId}</div>;
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+  const { description, title, date, location, image } = event || {};
+  if (!event) {
+    return <p>No event found</p>;
+  }
+  return (
+    <>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default DetailEventPage;
